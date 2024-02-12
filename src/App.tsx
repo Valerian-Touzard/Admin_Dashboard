ab09038f979f7db25fd7862d049477ed2ea6d7fc
vtouzard <vtouzard244@gmail.com>
2024-02-12 16:53:27 +0100

    feat: Ajout d'un layout pour pouvoir ajouter les différends menu plus simplement

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,7 @@ import routerBindings, {
 import { App as AntdApp } from "antd";
 import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
 import Layout from "./components/layout";
+import { ressources } from "./config/ressources";
 
 function App() {
   return (
@@ -30,6 +31,7 @@ function App() {
                 notificationProvider={useNotificationProvider}
                 routerProvider={routerBindings}
                 authProvider={authProvider}
+                resources={ressources}
                 options={{
                   syncWithLocation: true,
                   warnWhenUnsavedChanges: true,
