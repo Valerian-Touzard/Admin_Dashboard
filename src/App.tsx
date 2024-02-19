0f12ddd189142dba6b1df1c6296301ee428b072c
vtouzard <vtouzard244@gmail.com>
2024-02-19 12:47:57 +0100

    fix: Ré-indentation du code + correction de l'emplacement de la route pour le kanban

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,9 @@
-import { Authenticated, GitHubBanner, Refine, WelcomePage } from "@refinedev/core";
+import {
+  Authenticated,
+  GitHubBanner,
+  Refine,
+  WelcomePage,
+} from "@refinedev/core";
 import { DevtoolsPanel, DevtoolsProvider } from "@refinedev/devtools";
 import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
 
@@ -6,7 +11,7 @@ import { useNotificationProvider } from "@refinedev/antd";
 import "@refinedev/antd/dist/reset.css";
 
 import { authProvider, dataProvider, liveProvider } from "./providers";
-import { Home, ForgotPassword, Login, Register, CompanyList } from "./pages"
+import { Home, ForgotPassword, Login, Register, CompanyList } from "./pages";
 
 import routerBindings, {
   CatchAllNavigate,
@@ -26,57 +31,61 @@ function App() {
     <BrowserRouter>
       <GitHubBanner />
       <RefineKbarProvider>
-          <AntdApp>
-            <DevtoolsProvider>
-              <Refine
-                dataProvider={dataProvider}
-                liveProvider={liveProvider}
-                notificationProvider={useNotificationProvider}
-                routerProvider={routerBindings}
-                authProvider={authProvider}
-                resources={ressources}
-                options={{
-                  syncWithLocation: true,
-                  warnWhenUnsavedChanges: true,
-                  useNewQueryKeys: true,
-                  projectId: "NcWGO4-RnzJl2-v4k0vB",
-                  liveMode: "auto",
-                }}
-              >
-                <Routes>
-                  
-                  <Route path="/register" index element={<Register />} />
-                  <Route path="/login" index element={<Login />} />
-                  <Route path="/forgot-password" index element={<ForgotPassword />} />
-                  <Route 
-                    element={
-                    <Authenticated 
+        <AntdApp>
+          <DevtoolsProvider>
+            <Refine
+              dataProvider={dataProvider}
+              liveProvider={liveProvider}
+              notificationProvider={useNotificationProvider}
+              routerProvider={routerBindings}
+              authProvider={authProvider}
+              resources={ressources}
+              options={{
+                syncWithLocation: true,
+                warnWhenUnsavedChanges: true,
+                useNewQueryKeys: true,
+                projectId: "NcWGO4-RnzJl2-v4k0vB",
+                liveMode: "auto",
+              }}
+            >
+              <Routes>
+                <Route path="/register" index element={<Register />} />
+                <Route path="/login" index element={<Login />} />
+                <Route
+                  path="/forgot-password"
+                  index
+                  element={<ForgotPassword />}
+                />
+                <Route
+                  element={
+                    <Authenticated
                       key="authenticated-layout"
                       fallback={<CatchAllNavigate to="/login" />}
-                  >
-                    <Layout>
-                      <Outlet />
-                    </Layout>
-                  </Authenticated>
-                  }>
-                    <Route index element={<Home />} />
-                    <Route path="/companies">
-                      <Route index element={<CompanyList />}/>
-                      <Route path="new" element={<Create />} />
-                      <Route path="edit/:id" element={<EditPage />} />
-                    </Route>
+                    >
+                      <Layout>
+                        <Outlet />
+                      </Layout>
+                    </Authenticated>
+                  }
+                >
+                  <Route index element={<Home />} />
+                  <Route path="/companies">
+                    <Route index element={<CompanyList />} />
+                    <Route path="new" element={<Create />} />
+                    <Route path="edit/:id" element={<EditPage />} />
                   </Route>
                   <Route path="/tasks">
-                    <Route index element={<List />}/>
+                    <Route index element={<List />} />
                   </Route>
-                </Routes>
-                <RefineKbar />
-                <UnsavedChangesNotifier />
-                <DocumentTitleHandler />
-              </Refine>
-              <DevtoolsPanel />
-            </DevtoolsProvider>
-          </AntdApp>
+                </Route>
+              </Routes>
+              <RefineKbar />
+              <UnsavedChangesNotifier />
+              <DocumentTitleHandler />
+            </Refine>
+            <DevtoolsPanel />
+          </DevtoolsProvider>
+        </AntdApp>
       </RefineKbarProvider>
     </BrowserRouter>
   );
--- src/components/tasks/kanban/board.tsx
+++ src/components/tasks/kanban/board.tsx
@@ -4,9 +4,21 @@ import React from 'react'
 export const KanbanBoardContainer = ({ children }: React.PropsWithChildren) => {
   return (
     <div
-        style={{  }}
+        style={{ 
+            width: 'calc(100% + 64px)',
+            height: 'calc(100vh - 64px)',
+            display: 'flex',
+            flexDirection: 'column',
+            margin: '-32px'
+         }}
     >
-        <div style={{  }}>
+        <div style={{ 
+            width: '100%',
+            height: '100%',
+            display: 'flex',
+            padding: '32px',
+            overflow: 'scroll'
+         }}>
             {children}
         </div>
     </div>
