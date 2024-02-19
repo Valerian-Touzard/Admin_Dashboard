f3017fa76e8ff07b27644ce593d7815c0a2dedd4
vtouzard <vtouzard244@gmail.com>
2024-02-19 11:00:49 +0100

    feat: Ajout de la modification de la taille de l'entreprise

--- src/pages/company/edit.tsx
+++ src/pages/company/edit.tsx
@@ -1,72 +1,83 @@
-import { Col, Form, Row, Select } from 'antd'
-import { Edit, useForm, useSelect } from '@refinedev/antd'
-import { UPDATE_COMPANY_MUTATION } from '@/graphql/mutations'
-import CustomAvatar from '@/components/custom-avatar'
-import { getNameInitials } from '@/utilities'
-import { GetFieldsFromList } from '@refinedev/nestjs-query'
-import { UsersSelectQuery } from '@/graphql/types'
-import { USERS_SELECT_QUERY } from '@/graphql/queries'
-import SelectOptionWithAvatar from '@/components/select-option-with-avatar'
+import { Col, Form, Row, Select } from "antd";
+import { Edit, useForm, useSelect } from "@refinedev/antd";
+import { UPDATE_COMPANY_MUTATION } from "@/graphql/mutations";
+import CustomAvatar from "@/components/custom-avatar";
+import { getNameInitials } from "@/utilities";
+import { GetFieldsFromList } from "@refinedev/nestjs-query";
+import { UsersSelectQuery } from "@/graphql/types";
+import { USERS_SELECT_QUERY } from "@/graphql/queries";
+import SelectOptionWithAvatar from "@/components/select-option-with-avatar";
+import { companySizeOptions } from "@/constants";
 
 const EditPage = () => {
-    const { saveButtonProps, formProps, formLoading, queryResult } = useForm({
-        redirect: false,
-        meta:{
-            gqlMutation: UPDATE_COMPANY_MUTATION
-        }
-    });
-
-const { avatarUrl, name } = queryResult?.data?.data || {}
-
-const { selectProps, queryResult: queryResultUsers } = useSelect<GetFieldsFromList<UsersSelectQuery>>({
-    resource: "users",
-    optionLabel: 'name',
-    pagination: {
-        mode: 'off'
+  const { saveButtonProps, formProps, formLoading, queryResult } = useForm({
+    redirect: false,
+    meta: {
+      gqlMutation: UPDATE_COMPANY_MUTATION,
     },
-    meta:{
-        gqlQuery: USERS_SELECT_QUERY
-    }
-})
+  });
+
+  const { avatarUrl, name } = queryResult?.data?.data || {};
+
+  const { selectProps, queryResult: queryResultUsers } = useSelect<
+    GetFieldsFromList<UsersSelectQuery>
+  >({
+    resource: "users",
+    optionLabel: "name",
+    pagination: {
+      mode: "off",
+    },
+    meta: {
+      gqlQuery: USERS_SELECT_QUERY,
+    },
+  });
 
   return (
     <div>
-        <Row gutter={[32,32]}>
-            <Col xs={24} xl={12}>
-                <Edit 
-                    isLoading={formLoading}
-                    saveButtonProps={saveButtonProps}
-                    breadcrumb={false}
-                >
-                    <Form {...formProps} layout='vertical'>
-                        <CustomAvatar shape='square'src={avatarUrl} name={getNameInitials(name || '')} style={{width: 96, height: 96, marginBottom: '24px'}}/>
-                        <Form.Item
-                    label="Sales owner"
-                    name="salesOwnerId"
-                    initialValue={formProps?.initialValues?.salesOwner?.id}
-                >
-                    <Select
-                        placeholder="Please select a sale owner"
-                        {...selectProps}
-                        options={
-                            queryResultUsers.data?.data.map((user) => ({
-                                value: user.id,
-                                label: (
-                                    <SelectOptionWithAvatar 
-                                        name={user.name}
-                                        avatarUrl={user.avatarUrl ?? undefined}
-                                    />
-                                )
-                            })) ?? []
-                        }
-                    />
-                </Form.Item>
-                    </Form>
-                </Edit>
-            </Col>
-        </Row>
+      <Row gutter={[32, 32]}>
+        <Col xs={24} xl={12}>
+          <Edit
+            isLoading={formLoading}
+            saveButtonProps={saveButtonProps}
+            breadcrumb={false}
+          >
+            <Form {...formProps} layout="vertical">
+              <CustomAvatar
+                shape="square"
+                src={avatarUrl}
+                name={getNameInitials(name || "")}
+                style={{ width: 96, height: 96, marginBottom: "24px" }}
+              />
+              <Form.Item
+                label="Sales owner"
+                name="salesOwnerId"
+                initialValue={formProps?.initialValues?.salesOwner?.id}
+              >
+                <Select
+                  placeholder="Please select a sale owner"
+                  {...selectProps}
+                  options={
+                    queryResultUsers.data?.data.map((user) => ({
+                      value: user.id,
+                      label: (
+                        <SelectOptionWithAvatar
+                          name={user.name}
+                          avatarUrl={user.avatarUrl ?? undefined}
+                        />
+                      ),
+                    })) ?? []
+                  }
+                />
+              </Form.Item>
+              <Form.Item label="Company size">
+                <Select options={companySizeOptions} />
+              </Form.Item>
+            </Form>
+          </Edit>
+        </Col>
+      </Row>
     </div>
-  )
-}
+  );
+};
 
-export default EditPage
+export default EditPage;
